d112b7863cb5e39f58a7f6e55c94c4d923a39216
LastPatriot <oluwadareopeyemis1@gmail.com>
2026-02-18 22:20:17 +0100

    end of chapter 3

--- src/configuration.rs
+++ src/configuration.rs
@@ -3,11 +3,11 @@
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16
+    pub application_port: u16,
 }
 
 #[derive(serde::Deserialize)]
-pub struct DatabaseSettings{
+pub struct DatabaseSettings {
     pub username: String,
     pub password: String,
     pub port: u16,
@@ -16,15 +16,26 @@ pub struct DatabaseSettings{
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let settings =  config::Config::builder()
-        .add_source(config::File::new("configuration.yaml", config::FileFormat::Yaml)
-        )
+    let settings = config::Config::builder()
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
         .build()?;
     settings.try_deserialize::<Settings>()
 }
 
 impl DatabaseSettings {
-    pub fn connection_string(&self)->String{
-        format!("postgres://{}:{}@{}:{}/{}",self.username, self.password, self.host, self.port, self.database_name)
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
     }
 }
